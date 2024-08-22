8611fdd7e6f832d0fd6683f8efbfbf8cbfa319fc
Joanhey <joanhey@kumbiaphp.com>
2024-08-22 11:08:36 +0200

    Add Lumen v11

---
+++ lumen-11.0/_benchmark/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# create project
+rm -rf _benchmark/temp
+composer create-project --prefer-dist laravel/lumen:11.0.* ./_benchmark/temp --ansi
+mv ./_benchmark/temp/{.,}* ./
+
+# have the route & controller
+yes|cp -rf _benchmark/lumen/. ./
+
+# some enhancements
+composer install --no-dev -o --ansi
+chmod -R o+w storage
+rm ./public/.htaccess
